ee63a4a460fdbc309a3dc430f7ed8a2e7e49dbfd
Daniel Tesfagabr <danielhh2008@gmail.com>
2023-07-01 22:44:01 +0100

    Added new scaffolding, datatables and delete confirmation message

--- LMS/Data/LmsDbContext.cs
+++ LMS/Data/LmsDbContext.cs
@@ -15,14 +15,35 @@ public partial class LmsDbContext : DbContext
     {
     }
 
+    public virtual DbSet<Class> Classes { get; set; }
+
     public virtual DbSet<Course> Courses { get; set; }
+
+    public virtual DbSet<Enrollment> Enrollments { get; set; }
 
     public virtual DbSet<Lecturer> Lecturers { get; set; }
 
     public virtual DbSet<Student> Students { get; set; }
 
+    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+#warning To protect potentially sensitive information in your connection string, you should move it out of source code. You can avoid scaffolding the connection string by using the Name= syntax to read it from configuration - see https://go.microsoft.com/fwlink/?linkid=2131148. For more guidance on storing connection strings, see http://go.microsoft.com/fwlink/?LinkId=723263.
+        => optionsBuilder.UseSqlServer("Data Source=(localdb)\\MSSQLLocalDB;Initial Catalog=LmsDb");
+
     protected override void OnModelCreating(ModelBuilder modelBuilder)
     {
+        modelBuilder.Entity<Class>(entity =>
+        {
+            entity.HasKey(e => e.Id).HasName("PK__Classes__3214EC07FB1C11A3");
+
+            entity.HasOne(d => d.Course).WithMany(p => p.Classes)
+                .HasForeignKey(d => d.CourseId)
+                .HasConstraintName("FK__Classes__CourseI__3B75D760");
+
+            entity.HasOne(d => d.Lecturer).WithMany(p => p.Classes)
+                .HasForeignKey(d => d.LecturerId)
+                .HasConstraintName("FK__Classes__Lecture__3A81B327");
+        });
+
         modelBuilder.Entity<Course>(entity =>
         {
             entity.HasKey(e => e.Id).HasName("PK__Courses__3214EC0784527E65");
@@ -31,6 +52,21 @@ public partial class LmsDbContext : DbContext
 
             entity.Property(e => e.Code).HasMaxLength(5);
             entity.Property(e => e.Name).HasMaxLength(50);
+        });
+
+        modelBuilder.Entity<Enrollment>(entity =>
+        {
+            entity.HasKey(e => e.Id).HasName("PK__Enrollme__3214EC07CFBCD0C8");
+
+            entity.Property(e => e.Grade).HasMaxLength(2);
+
+            entity.HasOne(d => d.Class).WithMany(p => p.Enrollments)
+                .HasForeignKey(d => d.ClassId)
+                .HasConstraintName("FK__Enrollmen__Class__3F466844");
+
+            entity.HasOne(d => d.Student).WithMany(p => p.Enrollments)
+                .HasForeignKey(d => d.StudentId)
+                .HasConstraintName("FK__Enrollmen__Stude__3E52440B");
         });
 
         modelBuilder.Entity<Lecturer>(entity =>
---
+++ LMS/Models/StudentMetaData.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+
+namespace LMS.Data
+{
+    public class StudentMetaData
+    {
+        [DisplayName("First Name")]
+        public string FirstName { get; set; } = null!;
+        [DisplayName("Last Name")]
+        public string LastName { get; set; } = null!;
+        
+        [DisplayName("Date Of Birth")]
+        public DateTime? DateOfBirth { get; set; }
+    }
+    [ModelMetadataType(typeof(StudentMetaData))]
+    public partial class Student { }
+
+}
