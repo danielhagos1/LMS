9d8af46745e37b9d9ac66bfaec901cb099b2ba0e
Daniel Tesfagabr <danielhh2008@gmail.com>
2023-08-09 21:35:39 +0100

    added custome view model to get enrolled to the course

---
+++ LMS/Models/ClassEnrollmentViewModel.cs
@@ -0,0 +1,13 @@
+﻿using LMS.Data;
+
+namespace LMS.Models
+{
+    public class ClassEnrollmentViewModel
+    {
+        public Class? Class { get; set; }
+        public List<StudentEnrollmentViewModel> Enrollments { get; set; } = new
+            List<StudentEnrollmentViewModel>();
+
+    }
+
+}
---
+++ LMS/Models/StudentEnrollmentViewModel.cs
@@ -0,0 +1,7 @@
+﻿public class StudentEnrollmentViewModel
+{
+    public int Id { get; set; }
+    public string FirstName { get; set; }
+    public string LastName { get; set; }
+    public bool IsEnrolled { get; set; }
+}
